25d6cd5e45f63dec4c8220d44c0b49d44e08826f
likithareddygari <118160640+likithareddygari@users.noreply.github.com>
2026-06-13 16:25:55 +0530

    Update and rename Hello.java to Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+//testing jenkins
+public class Main {
+    public static void main(String[] args) {
+        // Print a simple greeting to the console
+        System.out.println("Hello, World!");
+
+        // Declare variables with specific data types
+        int numberOne = 15;
+        int numberTwo = 25;
+
+        // Perform a calculation
+        int summary = numberOne + numberTwo;
+
+        // Display results using string concatenation
+        System.out.println("The sum of " + numberOne + " and " + numberTwo + " is: " + summary);
+    }
+}
